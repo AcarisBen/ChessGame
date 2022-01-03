b11afe8eefe63955018e7abe0098fcce164c583e
Acaris <acaris.ben@gmail.com>
2022-01-03 12:31:09 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,11 +29,13 @@ public class ChessMatch { // Regras do jogo
 		}
 		return mat; // Por fim, este código retorna a matriz de peças da partida de xadrez.
 	}
-	
+	private void placeNewPiece(char column, int row, ChessPiece piece) { //Operação de colocar as peças. Recebe as coordenadas do xadrez.
+		board.placePiece(piece, new ChessPosition(column, row).toPosition()); //instancia a peça na posição baseada da matriz.
+	}
 	private void initialSetup() { //Assim que iniciará no tabuleiro
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1)); // Coloca a torre branca na posição
-		board.placePiece(new King(board, Color.BLACK), new Position(0, 4)); // Coloca a rei preto na posição
-		board.placePiece(new King(board, Color.WHITE), new Position(7, 4)); // Coloca o rei branco na posição
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE)); // Coloca a torre branca na posição
+		placeNewPiece('e', 8, new King(board, Color.BLACK)); // Coloca a rei preto na posição
+		placeNewPiece('e', 1,new King(board, Color.WHITE)); // Coloca o rei branco na posição
 	}
 }
 
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,39 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition { // Passa pro programa para ser informado uma letra e um número, convertendo para a posição na matriz
+
+	private char column;
+	private int row;
+	
+	
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {//Programação defensiva, caso seja informado algum valor fora do padrão do xadrez
+			// lembrando que é possível realizar operações com char. 'a'=1, 'b'=2...
+			throw new ChessException("Error instantiating chess position. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() { // Não tem setRow nem setColumn para não deixar alterar a esses valores.
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	protected Position toPosition() { // Dada na posição do tabuleiro, retorna uma posição da matriz.
+		return new Position(8 - row, column - 'a'); //8 - linha da matriz e coluna da matriz - char 'a'
+	}
+
+	protected static ChessPosition fromPosition(Position position) {// Dada a posição da matriz deve converter para a posição do xadrez 
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow()); //casting pra "char"(por não ser automático) e pega a posição da matriz e converte para a posição do xadrez
+	}
+
+	@Override
+	public String toString() { //imprime na tela a coluna e a linha. As "" na frente mostra pro programa que é tudo String.
+		return "" + column + row;
+	}
+}
